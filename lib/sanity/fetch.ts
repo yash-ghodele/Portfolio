cad4561d1633591b2aa3f7d1dded03d3ab8eb86d
Yash <160397817+yash-ghodele@users.noreply.github.com>
2026-05-10 22:07:34 +0530

    chore: complete Sanity CMS removal and repository cleanup

--- lib/sanity/fetch.ts
+++ lib/sanity/fetch.ts
@@ -1,52 +1,21 @@
-import { client } from "@/sanity/lib/client";
-import { projectsQuery, eventsQuery, projectBySlugQuery, eventBySlugQuery } from "./queries";
-import { ProjectMetadata, EventMetadata } from "@/lib/types";
 import { STATIC_PROJECTS } from "@/lib/projects";
-
-const fetchOptions = { next: { revalidate: 60 } } as any;
+import { STATIC_EVENTS } from "@/lib/events";
 
 export async function getProjects() {
-    const sanityProjects = await client.fetch(projectsQuery, {}, fetchOptions);
-    const mappedSanity = sanityProjects.map((p: any) => ({
-        slug: p.slug,
-        metadata: {
-            ...p,
-            tech: p.tech || [],
-        } as ProjectMetadata
-    }));
-
-    // Merge static and sanity, avoiding duplicates by slug
-    const allProjects = [...STATIC_PROJECTS];
-    mappedSanity.forEach((sp: any) => {
-        if (!allProjects.find(ap => ap.slug === sp.slug)) {
-            allProjects.push(sp);
-        }
-    });
-
-    return allProjects;
+    return STATIC_PROJECTS;
 }
 
 export async function getEvents() {
-    const events = await client.fetch(eventsQuery, {}, fetchOptions);
-    return events.map((e: any) => ({
-        slug: e.slug,
-        metadata: {
-            ...e,
-            metrics: e.metrics || [],
-            skills: e.skills || []
-        } as EventMetadata
-    }));
+    return STATIC_EVENTS;
 }
 
 export async function getProject(slug: string) {
-    // Check static first
-    const staticProject = STATIC_PROJECTS.find(p => p.slug === slug);
-    if (staticProject) return staticProject.metadata;
-
-    return await client.fetch(projectBySlugQuery, { slug }, fetchOptions);
+    const project = STATIC_PROJECTS.find(p => p.slug === slug);
+    return project ? project.metadata : null;
 }
 
 export async function getEvent(slug: string) {
-    return await client.fetch(eventBySlugQuery, { slug }, fetchOptions);
+    const event = STATIC_EVENTS.find(e => e.slug === slug);
+    return event ? event.metadata : null;
 }
 
--- sanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { project } from './project'
-import { event } from './event'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [project, event],
-}
